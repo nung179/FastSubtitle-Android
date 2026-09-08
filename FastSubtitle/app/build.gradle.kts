1d49cd021efa0f6ec709897099af306f41df8094
nung179 <120352038+nung179@users.noreply.github.com>
2026-09-08 14:15:22 +0700

    Update build.gradle.kts

--- FastSubtitle/app/build.gradle.kts
+++ FastSubtitle/app/build.gradle.kts
@@ -11,10 +11,8 @@ android {
         applicationId = "com.fastsubtitle.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        versionCode = 2
+        versionName = "1.0.0"
     }
 
     compileOptions {
@@ -25,22 +23,11 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.mlkit:translate:17.0.3")
 }
